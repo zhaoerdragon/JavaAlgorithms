ef68d50bba49b84207d5cd6b38a0cdd3a1b1aa47
WillScorpio <zhao38276543@126.com>
2018-08-13 00:36:07 +0800

    一次tt

--- src/com/xupt/willscorpio/cowguest/OfferAlgorithm.java
+++ src/com/xupt/willscorpio/cowguest/OfferAlgorithm.java
@@ -2,30 +2,10 @@ package com.xupt.willscorpio.cowguest;
 
 import com.xupt.willscorpio.datastructute.ListNode;
 
-import java.util.HashSet;
+import java.util.*;
+
 
 public class OfferAlgorithm {
-
-    public static void main(String[] args) {
-        TreeNode root = new TreeNode(0);
-        TreeNode root1 = new TreeNode(1);
-        TreeNode root2 = new TreeNode(2);
-        TreeNode root3 = new TreeNode(3);
-        TreeNode root4 = new TreeNode(4);
-        TreeNode root5 = new TreeNode(5);
-        TreeNode root6 = new TreeNode(6);
-        root.left = root1;
-        root1.left = root2;
-        root2.left = root3;
-
-        /*root.right = root4;
-        root4.left = root5;
-        root4.right = root6;*/
-
-        OfferAlgorithm offerAlgorithm = new OfferAlgorithm();
-        boolean answer = offerAlgorithm.IsBalanced_Solution(root);
-        System.out.println(answer);
-    }
 
     /**
      * 输入一棵二叉树，判断该二叉树是否是平衡二叉树。
@@ -100,5 +80,205 @@ public class OfferAlgorithm {
                 getSymmetrical(left.left, right.right) &&
                 getSymmetrical(left.right, right.left);
     }
+
+    /**
+     * 给定一个由不同的小写字母组成的字符串，输出这个字符串的所有全排列。
+     * 我们假设对于小写字母有'a' < 'b' < ... < 'y' < 'z'，而且给定的字符串中的字母已经按照从小到大的顺序排列。
+     */
+    protected static void quanPaiLie() {
+        Scanner scanner = new Scanner(System.in);
+        String string = scanner.nextLine();
+        char[] chars = string.toCharArray();
+        Arrays.sort(chars);
+
+        Set<String> stringSet = paiLie(chars, 0);
+        Iterator iterator = stringSet.iterator();
+        while (iterator.hasNext()) {
+            System.out.println(iterator.next());
+        }
+        System.out.println();
+    }
+
+    private static Set<String> paiLie(char[] chars, int start) {
+        Set<String> stringSet = new TreeSet<>();
+        if (start == chars.length - 1) {
+            stringSet.add(String.valueOf(chars));
+            return stringSet;
+        }
+        for (int j = start; j < chars.length; j++) {
+            swap(chars, start, j);
+            stringSet.addAll(paiLie(chars, start + 1));
+            swap(chars, start, j);
+        }
+        return stringSet;
+    }
+
+    private static void swap(char[] chars, int x, int y) {
+        char temp = chars[x];
+        chars[x] = chars[y];
+        chars[y] = temp;
+    }
+
+    /**
+     * 会下国际象棋的人都很清楚：皇后可以在横、竖、斜线上不限步数地吃掉其他棋子。如何将 8 个皇后放在棋盘上（有8×8个方格），使它们谁也不能被吃掉
+     * ！这就是著名的八皇后问题。
+     * 对于某个满足要求的8皇后的摆放方法，定义一个皇后串a与之对应，即 a=b1b2...b8, 其中bi（1≤bi≤8）为相应摆法中第 i 行皇后所处的列数。已经知
+     * 道8皇后问题一共有92组解（即92个不同的皇后串）。给出一个数n，要求输出第n个串。串的比较是这样的:皇后串x置于皇后串y之前，当且仅当将x视为整数
+     * 时比y小。
+     */
+    public void eightHuangHou() {
+        Scanner scanner = new Scanner(System.in);
+        List<Integer> integerList = new ArrayList<>();
+        while (scanner.hasNextInt()) {
+            integerList.add(scanner.nextInt());
+        }
+
+    }
+
+    /**
+     * 统计一个数字在排序数组中出现的次数。
+     *
+     * @param array
+     * @param k
+     * @return
+     */
+    public int GetNumberOfK(int[] array, int k) {
+        int sum = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == k) {
+                sum++;
+            }
+            if (array[i] != k && sum != 0) {
+                return sum;
+            }
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        OfferAlgorithm offerAlgorithm = new OfferAlgorithm();
+        int answer = offerAlgorithm.GetNumberOfK1(new int[]{1, 2, 2, 3}, 2);
+        System.out.println(answer);
+    }
+
+    public int GetNumberOfK1(int[] array, int k) {
+        return searchK(array, k + 0.5f) - searchK(array, k - 0.5f);
+    }
+
+    private int searchK(int[] array, float k) {
+        int start = 0;
+        int end = array.length - 1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (array[mid] < k) {
+                start = mid + 1;
+            }else if(array[mid] > k){
+                end = mid - 1;
+            }
+        }
+        return start;
+    }
+
+    /**
+     * 输入n个整数，找出其中最小的K个数。例如输入4,5,1,6,2,7,3,8这8个数字，则最小的4个数字是1,2,3,4,。
+     * @param input
+     * @param k
+     * @return
+     */
+    public ArrayList<Integer> GetLeastNumbers_Solution(int [] input, int k) {
+        if (k > input.length || input.length == 0) {
+            return new ArrayList<Integer>();
+        }
+        for (int i = (input.length - 1) / 2; i >= 0; i--) {
+            adjustHeap(input, i, input.length - 1);
+        }
+
+        for (int i = input.length - 1; i >= input.length - k; i--) {
+            int temp = input[0];
+            input[0] = input[i];
+            input[i] = temp;
+            adjustHeap(input, 0, i);
+        }
+        ArrayList<Integer> integers = new ArrayList<>();
+        for (int i = input.length - 1; i >= input.length - k; i--) {
+            integers.add(input[i]);
+        }
+
+        return integers;
+    }
+
+    public void adjustHeap(int[] array, int i, int length) {
+        int temp = array[i];
+        for (int k = 2 * i + 1; k < length; k = 2 * k + 1) {
+            if (k + 1 < length && array[k + 1] < array[k]) {
+                k++;
+            }
+            if (array[k] < temp) {
+                array[i] = array[k];
+                i = k;
+            }else {
+                break;
+            }
+        }
+        array[i] = temp;
+    }
+
+    /**
+     * 一个整型数组里除了两个数字之外，其他的数字都出现了偶数次。请写程序找出这两个只出现一次的数字。
+     * num1,num2分别为长度为1的数组。传出参数
+     * 将num1[0],num2[0]设置为返回结果
+     * @param array
+     * @param num1
+     * @param num2
+     */
+    public void FindNumsAppearOnce(int [] array,int num1[] , int num2[]) {
+
+    }
+
+    /**
+     * 在一个字符串(0<=字符串长度<=10000，全部由字母组成)中找到第一个只出现一次的字符,并返回它的位置, 如果没有则返回 -1（需要区分大小写）.
+     *
+     * @param str
+     * @return
+     */
+    public int FirstNotRepeatingChar(String str) {
+        if (str == null || str.length() == 0) {
+            return -1;
+        }
+        HashMap<Character, Integer> hashMap = new HashMap<>();
+        for (int i = 0; i < str.length(); i++) {
+            if (hashMap.containsKey(str.charAt(i))) {
+                int value = hashMap.get(str.charAt(i));
+                value++;
+                hashMap.put(str.charAt(i), value);
+            } else {
+                hashMap.put(str.charAt(i), 1);
+            }
+        }
+        for (int i = 0; i < str.length(); i++) {
+            if (hashMap.get(str.charAt(i)) == 1) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    /**
+     * 输入一个字符串,按字典序打印出该字符串中字符的所有排列。例如输入字符串abc,则打印出由字符a,b,c所能排列出来的所有字符串
+     * abc,acb,bac,bca,cab和cba。
+     * @param str
+     * @return
+     */
+    public ArrayList<String> Permutation(String str) {
+        if (str == null || str.length() == 0) {
+            return null;
+        }
+
+    }
+
+    private ArrayList<String> permuta(String string, int length) {
+
+    }
+
 
 }
