1be98e29d3b418989560e78fcd603618153571ea
willscorpio <zhao38276543@126.com>
2019-03-04 11:11:00 +0800

    修改一些文件

--- src/com/xupt/willscorpio/MainFunction.java
+++ src/com/xupt/willscorpio/MainFunction.java
@@ -3,7 +3,6 @@ package com.xupt.willscorpio;
 public class MainFunction{
 
     public static void main(String[] args) {
-        String string = "leetcode";
         System.out.println("123");
     }
 }
